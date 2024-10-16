3f8b45be44b04f598b9ad904b1553d54259a5591
Fateskink <shaynenguyen9@gmail.com>
2024-10-16 21:18:32 +0700

    add head file

--- tcp.c
+++ tcp.c
@@ -1,14 +1,4 @@
-#include <netinet/in.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define IP "87.248.100.216" /* yahoo.com */
-#define PORT 80
+#include <tcp.h>
 
 int main()
 {
--- tcp.h
+++ tcp.h
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
+#define IP "87.248.100.216" /* yahoo.com */
+#define PORT 80
+
+int main();
